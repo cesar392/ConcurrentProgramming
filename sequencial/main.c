187a63a9765e70ee9ac7bb2d8e0028bf491ef5d4
Cesar Comelli <cesar.comelli@dynamox.net>
2023-05-20 20:52:52 -0300

    WIP

--- sequencial/main.c
+++ sequencial/main.c
@@ -1,17 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <pthread.h>
 #include "gol.h"
+
+typedef struct {
+    int size;
+    int steps;
+    cell_t **prev;
+    cell_t **next;
+    stats_t stats_step;
+} thread_data_t;
+
+pthread_mutex_t stats_mutex;
+sem_t step_semaphore;
+int current_step = 0;
+stats_t stats_total = {0, 0, 0, 0};  // Definir stats_total no escopo global
+
+void *play_parallel(void *arg)
+{
+    thread_data_t *data = (thread_data_t *)arg;
+
+    while (1) {
+        sem_wait(&step_semaphore);
+
+        if (current_step >= data->steps) {
+            sem_post(&step_semaphore);
+            break;
+        }
+
+        int step = current_step++;
+        data->stats_step = play(data->prev, data->next, data->size);
+
+        pthread_mutex_lock(&stats_mutex);
+        stats_total.borns += data->stats_step.borns;
+        stats_total.survivals += data->stats_step.survivals;
+        stats_total.loneliness += data->stats_step.loneliness;
+        stats_total.overcrowding += data->stats_step.overcrowding;
+        pthread_mutex_unlock(&stats_mutex);
+
+        printf("Step %d ----------\n", step + 1);
+        print_board(data->next, data->size);
+        print_stats(data->stats_step);
+
+        cell_t **tmp = data->next;
+        data->next = data->prev;
+        data->prev = tmp;
+
+        sem_post(&step_semaphore);
+    }
+
+    pthread_exit(NULL);
+}
 
 int main(int argc, char **argv)
 {
     int size, steps;
-    cell_t **prev, **next, **tmp;
+    cell_t **prev, **next;
     FILE *f;
-    stats_t stats_step = {0, 0, 0, 0};
-    stats_t stats_total = {0, 0, 0, 0};
 
-    if (argc != 2)
+    if (argc != 3)
     {
-        printf("ERRO! Você deve digitar %s <nome do arquivo do tabuleiro>!\n\n", argv[0]);
+        printf("ERRO! Você deve digitar %s <nome do arquivo do tabuleiro> <número de threads>!\n\n", argv[0]);
         return 0;
     }
 
@@ -30,37 +80,39 @@ int main(int argc, char **argv)
 
     fclose(f);
 
-#ifdef DEBUG
-    printf("Initial:\n");
-    print_board(prev, size);
-    print_stats(stats_step);
-#endif
+    int num_threads = atoi(argv[2]);
+    pthread_t *threads = (pthread_t *)malloc(sizeof(pthread_t) * num_threads);
+    thread_data_t *thread_data = (thread_data_t *)malloc(sizeof(thread_data_t) * num_threads);
 
-    for (int i = 0; i < steps; i++)
-    {
-        stats_step = play(prev, next, size);
-        
-        stats_total.borns += stats_step.borns;
-        stats_total.survivals += stats_step.survivals;
-        stats_total.loneliness += stats_step.loneliness;
-        stats_total.overcrowding += stats_step.overcrowding;
+    pthread_mutex_init(&stats_mutex, NULL);
+    sem_init(&step_semaphore, 0, 1);
 
-#ifdef DEBUG
-        printf("Step %d ----------\n", i + 1);
-        print_board(next, size);
-        print_stats(stats_step);
-#endif
-        tmp = next;
-        next = prev;
-        prev = tmp;
+    for (int i = 0; i < num_threads; i++) {
+        thread_data[i].size = size;
+        thread_data[i].steps = steps;
+        thread_data[i].prev = prev;
+        thread_data[i].next = next;
+        thread_data[i].stats_step = (stats_t){0, 0, 0, 0};
+        pthread_create(&threads[i], NULL, play_parallel, &thread_data[i]);
+    }
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
     }
 
 #ifdef RESULT
     printf("Final:\n");
-    print_board(prev, size);
-    print_stats(stats_total);
+    print_board(thread_data->prev, thread_data->size);
+    print_stats(thread_data->stats_step);
 #endif
 
+    free(threads);
+    free(thread_data);
     free_board(prev, size);
     free_board(next, size);
+
+    pthread_mutex_destroy(&stats_mutex);
+    sem_destroy(&step_semaphore);
+
+    return 0;
 }
