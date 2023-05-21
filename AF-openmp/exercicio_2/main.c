d9ae35ff9da4b507d2ad5f0c84e6672452e2ec3a
Cesar Comelli <cesar.comelli@dynamox.net>
2023-05-21 17:41:36 -0300

    ex2 solution

--- AF-openmp/exercicio_2/main.c
+++ AF-openmp/exercicio_2/main.c
@@ -10,16 +10,13 @@ void init_matrix(double* m, int rows, int columns) {
             m[i*columns+j] = i + j;
 }
 
-
 void mult_matrix(double* out, double* left, double *right, 
                  int rows_left, int cols_left, int cols_right) {
-    int i, j, k;
-    #pragma omp parallel for schedule(dynamic, 1)
-    for (i = 0; i < rows_left; ++i) {
-        for (j = 0; j < cols_right; ++j) {
+    #pragma omp parallel for collapse(2) schedule(static)
+    for (int i = 0; i < rows_left; ++i) {
+        for (int j = 0; j < cols_right; ++j) {
             out[i*cols_right+j] = 0;
-            #pragma omp parallel for firstprivate(i, j) schedule(guided)
-            for (k = 0; k < cols_left; ++k) 
+            for (int k = 0; k < cols_left; ++k) 
                 out[i*cols_right+j] += left[i*cols_left+k]*right[k*cols_right+j];
         }
     }
